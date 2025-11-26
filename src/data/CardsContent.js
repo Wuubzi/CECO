1ba3f16842b808344e6c861d0eb68a5b22c803f3
wuubzi <carlosasalas321@gmail.com>
2025-11-25 21:15:44 -0500

    adding unfinished project

---
+++ src/data/CardsContent.js
@@ -0,0 +1,47 @@
+import cards1 from "../assets/cards1.svg";
+import cards2 from "../assets/cards2.svg";
+import cards3 from "../assets/cards3.svg";
+import cards4 from "../assets/cards4.svg";
+import cards5 from "../assets/cards5.svg";
+import cards6 from "../assets/cards6.svg";
+
+const CardsContent = [
+    {
+        icon: cards1,
+        title: "Enfoque Personalizado",
+        description:
+            "Adaptamos nuestras estrategias de enseñanza para satisfacer las necesidades individuales de cada estudiante, asegurando un aprendizaje efectivo y significativo.",
+    },
+    {
+        icon: cards2,
+        title: "Docentes Especializados",
+        description:
+            "Contamos con educadores apasionados y cualificados, expertos en la metodología por ciclos, creando un entorno de apoyo que fomenta la curiosidad y la autonomía.",
+    },
+    {
+        icon: cards3,
+        title: "Desarrollo Socioemocional",
+        description:
+            "Más allá de lo académico, priorizamos la seguridad y proporcionamos un ambiente cálido que nutre el crecimiento social y emocional, fundamental en cada ciclo de vida.",
+    },
+    {
+        icon: cards4,
+        title: "Metodología Activa",
+        description:
+            "Implementamos estrategias de aprendizaje activas y lúdicas que fomentan la creatividad, la resolución de problemas y la imaginación, ajustadas a las competencias de cada ciclo.",
+    },
+    {
+        icon: cards5,
+        title: "Atención Personalizada",
+        description:
+            "Gracias a la estructura por ciclos, ofrecemos atención personalizada y flexibilidad curricular, permitiendo que cada estudiante avance y refuerce conocimientos a su propio ritmo.",
+    },
+    {
+        icon: cards6,
+        title: "Acompañamiento Integral y Soporte",
+        description:
+            "Ofrecemos una red de apoyo constante que incluye tutorías, flexibilidad y seguimiento continuo para garantizar que cada estudiante alcance con éxito sus metas académicas y profesionales.",
+    }
+];
+
+export default CardsContent;
