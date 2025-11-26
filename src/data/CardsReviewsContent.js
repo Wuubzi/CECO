ba9dee9f41842e41a6c281d5999e6a6ac41b5a18
wuubzi <carlosasalas321@gmail.com>
2025-11-26 03:36:56 -0500

    a

---
+++ src/data/CardsReviewsContent.js
@@ -0,0 +1,12 @@
+import reviewsFirst from "../assets/reviews-first.png";
+
+const CardsReviewsContent = [
+    {
+        image: reviewsFirst,
+        name: "Jennifer B",
+        rating: 5,
+        details: "Elegir CECO ha sido la mejor decisión para mi desarrollo profesional. El modelo por ciclos me permitió retomar mis estudios, avanzar a mi propio ritmo y consolidar mi conocimiento, abriéndome puertas que antes estaban cerradas. El acompañamiento de los tutores es excepcional."
+    }
+];
+
+export default CardsReviewsContent;
